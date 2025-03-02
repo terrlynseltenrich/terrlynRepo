4936a34c126b239eb1fd1c7fed873a7c45a85206
Terrlyn <terrie.seltenrich@outlook.com>
2025-03-02 07:07:40 +0000

    Test file complete

---
+++ TestFile.cpp
@@ -0,0 +1,6 @@
+#include <iostream> // Header for input/output
+
+int main() { //entry point of a C++ program - code inside main is first to be executed 
+    std::cout <<"These are the basics of C++"; // Console Out - Print function
+    return 0; //Return Statement
+}
