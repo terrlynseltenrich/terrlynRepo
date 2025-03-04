85f2519ee34477952b9ad3e882a8cffa290a6b78
terrlynseltenrich <terrieseltenrich@gmail.com>
2025-03-03 17:34:51 -0700

    Delete .vscode directory

--- .vscode/C++Basics.cpp
+++
@@ -1,6 +0,0 @@
-#include <iostream> // Header for input/output
-
-int main() { //entry point of a C++ program - code inside main is first to be executed 
-    std::cout <<"These are the basics of C++"; // Console Out - Print function
-    return 0; //Return Statement
-}
